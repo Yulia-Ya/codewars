7c2193f0ba6f5d62646f5239b1b8ec791ef07c3e
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2023-12-13 20:06:27 +0300

    caltulate Average

---
+++ 8kyu/calculateAvarage.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+//SOLUTION
+
+function find_average(arr) {
+    return arr.length > 0? arr.reduce((a, b) => a + b) / arr.length : 0;
+}
