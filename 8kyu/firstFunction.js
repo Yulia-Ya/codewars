3b211bf97fb60b318591491c8835b8f78eb1c579
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2023-12-19 19:27:56 +0300

    first function

---
+++ 8kyu/firstFunction.js
@@ -0,0 +1,10 @@
+//DESCRIPTION:
+
+//In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+//SOLUTION
+
+function helloWorld(){
+    let str = "Hello World!";
+    console.log(str);
+  }
