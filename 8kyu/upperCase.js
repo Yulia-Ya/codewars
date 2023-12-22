bcfa56ac3a91a8d68813f9b08854ed7219f4b40e
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2023-12-22 23:19:00 +0300

    upper case

---
+++ 8kyu/upperCase.js
@@ -0,0 +1,10 @@
+//DESCRIPTION:
+
+//Write a function which converts the input string to uppercase.
+
+//SOLUTION
+
+function makeUpperCase(str) {
+    str = str.toUpperCase();
+      return str;
+    }
