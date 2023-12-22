e48d2e0678d2410761c84c8fd87280eaf36ddbf6
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2023-12-22 23:21:49 +0300

    num to arr

---
+++ 8kyu/numToArr.js
@@ -0,0 +1,11 @@
+//DESCRIPTION:
+
+//Convert number to reversed array of digits
+
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(n) {
+    let arr = String(n).split('').reverse().map(Number);
+  return arr;
+  }
+  
