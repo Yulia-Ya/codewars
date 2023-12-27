862d83ee3aa7fa8c2019431c2b16251e273eefe0
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2023-12-27 22:49:46 +0300

    reversed

---
+++ 8kyu/reversedWords.js
@@ -0,0 +1,12 @@
+//DESCRIPTION:
+
+//Complete the solution so that it reverses all of the words within the string passed in.
+
+//Words are separated by exactly one space and there are no leading or trailing spaces.
+
+//SOLUTION
+
+function reverseWords(str){
+    str = str.split(' ').reverse().join(' ');
+    return str; // reverse those words
+  }
