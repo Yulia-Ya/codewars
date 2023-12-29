9f4856fd4c47495beddf2f5a8116e0eab3afea78
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2023-12-29 22:10:56 +0300

    make negative-1

--- 8kyu/returnNegative.js
+++ 8kyu/returnNegative.js
@@ -2,6 +2,9 @@
 
 //In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
+//SOLUTION
+
+
 function makeNegative(num) {
     if (num > 0) {
       return (num * (-1));
@@ -9,3 +12,5 @@ function makeNegative(num) {
       return num;
     }
   }
+
+  console.log(makeNegative(9));
