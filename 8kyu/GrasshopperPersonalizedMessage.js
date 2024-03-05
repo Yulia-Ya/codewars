b357d96575fe959f83db22ba13393a23f4314c52
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-03-05 21:29:22 +0300

    greet-1

--- 8kyu/GrasshopperPersonalizedMessage.js
+++ 8kyu/GrasshopperPersonalizedMessage.js
@@ -1,3 +1,7 @@
+//DESCRIPTION:
+
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
 function greet (name, owner) {
     if (name === owner){
       return 'Hello boss'
