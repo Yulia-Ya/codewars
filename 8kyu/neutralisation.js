ab6b6902e7e25120aa2196c1dad44eb034a44c55
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-03-11 16:38:50 +0300

    complited task disc added

--- 8kyu/neutralisation.js
+++ 8kyu/neutralisation.js
@@ -1,3 +1,19 @@
+// DESCRIPTION:
+
+// Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+// When positives and positives interact, they remain positive.
+// When negatives and negatives interact, they remain negative.
+// But when negatives and positives interact, they become neutral, and are shown as the number 0.
+// Worked Example
+
+// ("+-+", "+--") ➞ "+-0"
+// # Compare the first characters of each string, then the next in turn.
+// # "+" against a "+" returns another "+".
+// # "-" against a "-" returns another "-".
+// # "+" against a "-" returns "0".
+// # Return the string of characters.
+
 function neutralise(s1, s2) {
     // Here be dragons!
    let arrS1 = s1.split('');
@@ -23,3 +39,4 @@ function neutralise(s1, s2) {
   
   
 console.log(neutralise("-+-+-+", "-+-+-+"))
+console.log(neutralise("--++--", "++--++")) //"000000"
