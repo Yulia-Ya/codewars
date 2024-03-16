d1b0a29f9f949b5f56802ac1ab36a029a32eba6a
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-03-16 23:15:50 +0300

    desc added

--- 8kyu/findThePos.js
+++ 8kyu/findThePos.js
@@ -1,3 +1,11 @@
+// DESCRIPTION:
+
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
 function position(letter){
     let position =   'abcdefghijklmnopqrstuvwxyz'.split('').indexOf(letter) + 1
     return `Position of alphabet: ${position}`
