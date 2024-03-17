a61d1d936c709067f18ae20f4b3366c88acc29c6
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-03-17 23:48:23 +0300

    combat game disc

--- 8kyu/grasshopperTerminalGameCombatFunction.js
+++ 8kyu/grasshopperTerminalGameCombatFunction.js
@@ -1,3 +1,8 @@
+// DESCRIPTION:
+
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health.
+//  Health can't be less than 0.
+
 function combat(health, damage) {
     return health < damage ? 0 :  health - damage
    }
