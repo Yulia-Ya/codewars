5a07a879b3e617f730f2646a8051fb2b5b40d822
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-03-22 19:18:20 +0300

    desc added

--- 6kyu/sortTheOdd.js
+++ 6kyu/sortTheOdd.js
@@ -1,3 +1,8 @@
+// Description
+
+// You will be given an array of numbers.
+//  You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
 function sortArray(array) {
     const arrOdd =array.filter( el => el % 2 !== 0).sort((a, b) => a - b)
    return array.map( el => el % 2 !== 0 ?  arrOdd.shift() : el)
