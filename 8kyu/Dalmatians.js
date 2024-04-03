46393d3336bbb508e9feb2f96620177b0000ed78
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-04-03 22:25:37 +0300

    disc added

--- 8kyu/Dalmatians.js
+++ 8kyu/Dalmatians.js
@@ -1,3 +1,11 @@
+// DESCRIPTION:
+
+// Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+
+// By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+
+// The number of dogs will always be a number and there will always be at least 1 dog.
+
 function howManyDalmatians(number){
     if (number <= 10) {
       return "Hardly any"
