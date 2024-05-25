96294a176bb98a4ba3a0a6975dbfde91cb73d8ba
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-05-25 23:18:57 +0300

    greeting-2

--- 8kyu/welcome.js
+++ 8kyu/welcome.js
@@ -21,3 +21,28 @@ var langs = {
 'swedish': 'Valkommen',
 'welsh': 'Croeso'
 };
+
+//2
+
+function greet(language) {
+	var welcomes = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  };
+  return language in welcomes ? welcomes[language] : welcomes.english;
+};
