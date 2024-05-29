eb6bc0e76ae0268b532d5d5a9ea73c65a31015ca
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-05-29 21:37:51 +0300

    high and low num

--- 7kyu/рighestAndLowest.js
+++ 7kyu/рighestAndLowest.js
@@ -1,4 +1,4 @@
-function highAndLow(numbers){
+ function highAndLow(numbers){
     const sorted =  numbers.split(' ').map(el => +el).sort((a,b) => a-b)
     return `${sorted[sorted.length-1]} ${sorted[0]}`
   }
