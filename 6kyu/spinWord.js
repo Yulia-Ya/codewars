42fedbaf9a920e191859eb7154ff8876c1227d04
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-06-30 21:20:51 +0300

    2nd version of spinword

--- 6kyu/spinWord.js
+++ 6kyu/spinWord.js
@@ -5,5 +5,12 @@ function spinWords(string){
     
 }
   
+function spinWords(string){
+  //TODO Have fun :)
   
+  const reversed =(word)=>{
+    return word.split('').reverse().join('')
+  } 
+  return string.split(' ').map(el => el.length >= 5 ? reversed(el): el).join(' ')
+}
   console.log(spinWords("This is another test"))
