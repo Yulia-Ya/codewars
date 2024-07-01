514276b26a7d976cd0826ed5bce864d77aa47e3a
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-07-01 16:26:25 +0300

    alphabetPosition 2ns version

--- 6kyu/ReplaceWithAlphabetPosition.js
+++ 6kyu/ReplaceWithAlphabetPosition.js
@@ -14,17 +14,27 @@
 //Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )//
 
 function alphabetPosition(text) {
-    let arr= []
-  let alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
-  let sign = text.toLowerCase()
- 
+  let arr = [];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  let sign = text.toLowerCase();
 
-  for(let i= 0; i <= text.length; i++){
-    if(alphabet.includes(sign[i]) ){
-      arr.push(alphabet.indexOf(sign[i]))
+  for (let i = 0; i <= text.length; i++) {
+    if (alphabet.includes(sign[i])) {
+      arr.push(alphabet.indexOf(sign[i]));
     }
   }
-  return arr.map( el => el + 1).join(' ')
-  }
-  
-  console.log(alphabetPosition("The sunset sets at twelve o' clock."))
+  return arr.map((el) => el + 1).join(" ");
+}
+
+function alphabetPosition(text) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((el) => alphabet.indexOf(el) !== -1)
+    .map((el) => alphabet.indexOf(el) + 1)
+    .join(" ");
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
