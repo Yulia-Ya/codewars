2ca7342f7c430dc1e8b36a745ea9d8d0d25a4947
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-07-04 19:24:11 +0300

    capitals desc added

--- 7kyu/findCapitals.js
+++ 7kyu/findCapitals.js
@@ -1,3 +1,16 @@
+// DESCRIPTION:
+
+// Instructions
+
+// Write a function that takes a single non-empty string 
+// of only lowercase and uppercase ascii letters (word)
+//  as its argument, and returns an ordered list containing
+//   the indices of all capital (uppercase) letters in the string.
+
+// Example (Input --> Output)
+
+// "CodEWaRs" --> [0,3,4,6]
+
 var capitals = function (word) {
 	// Write your code here
   let arr = []
