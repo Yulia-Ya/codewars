1d36d4fbbada189ca4a6701204a324a97364eca6
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-07-06 21:59:12 +0300

    capitals 2ns decision

--- 7kyu/findCapitals.js
+++ 7kyu/findCapitals.js
@@ -11,13 +11,19 @@
 
 // "CodEWaRs" --> [0,3,4,6]
 
+// var capitals = function (word) {
+// 	// Write your code here
+//   let arr = []
+//   for(let i = 0; i< word.length; i++){
+//      if(word[i] ===word[i].toUpperCase()){
+//        arr.push(i) 
+//      }
+//   }
+//  return arr
+// };
 var capitals = function (word) {
 	// Write your code here
-  let arr = []
-  for(let i = 0; i< word.length; i++){
-     if(word[i] ===word[i].toUpperCase()){
-       arr.push(i) 
-     }
-  }
- return arr
+  return word.split('').map(function(el){ if(el === el.toUpperCase()) {
+    return word.indexOf(el)
+  }}).filter(el=> el !== undefined)
 };
