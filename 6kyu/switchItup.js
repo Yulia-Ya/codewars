b86ff83c5013f5418bc719ab24f36e876cabb0a3
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-07-09 23:27:49 +0300

    switch

---
+++ 6kyu/switchItup.js
@@ -0,0 +1,7 @@
+function switchItUp(number){
+    //Write your own Code!
+      const numbwrWords = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six','Seven', 'Eight', 'Nine']
+    
+    return numbwrWords[number]
+    
+    }
