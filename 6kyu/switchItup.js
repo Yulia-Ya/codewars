3ca80440c418ae421d0dd4d24ec7ff9b30bbf67a
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-07-10 22:37:54 +0300

    2nd version

--- 6kyu/switchItup.js
+++ 6kyu/switchItup.js
@@ -5,3 +5,39 @@ function switchItUp(number){
     return numbwrWords[number]
     
     }
+
+    function switchItUp(number){
+      //Write your own Code!
+        switch (number) {
+        case 1:
+         return 'One'
+          break;
+           case 2:
+         return 'Two'
+          break;
+           case 3:
+         return 'Three'
+          break;
+           case 4:
+         return 'Four'
+          break;
+           case 5:
+         return 'Five'
+          break;
+           case 6:
+         return 'Six'
+          break;
+           case 7:
+         return 'Seven'
+          break;
+           case 8:
+         return 'Eight'
+          break;
+           case 9:
+         return 'Nine'
+          break;
+        
+        default:
+          return 'Zero'
+      }
+      }
