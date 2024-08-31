d5c867113cb4d29d8cf1812018da7b3073fe9b6d
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-08-31 21:43:42 +0300

    outliner added

---
+++ 6kyu/findOutlier.js
@@ -0,0 +1,5 @@
+function findOutlier(integers){
+    let odd =  integers.filter(el => el % 2 === 0)
+    let even =  integers.filter(el => el % 2 !== 0)
+  return odd.length > even.length ? even : odd
+  }
