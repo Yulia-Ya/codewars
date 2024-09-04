9846f6281574bb076637385c8cf191f9b18b9338
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-09-04 22:58:39 +0300

    desc added

--- 6kyu/findOutlier.js
+++ 6kyu/findOutlier.js
@@ -1,3 +1,10 @@
+// Description:
+// You are given an array (which will have a length of at least 3, 
+//   but could be very large) containing integers. 
+//   The array is either entirely comprised of odd integers or entirely comprised 
+//   of even integers except for a single integer N. Write a method that takes the 
+//   array as an argument and returns this "outlier" N.
+
 function findOutlier(integers){
     let odd =  integers.filter(el => el % 2 === 0)
     let even =  integers.filter(el => el % 2 !== 0)
